7627b0f6bb5e190dabe4c1cdc92a0f194463a999
arnoldomartinez3-source <arnoldomartinez3@gmail.com>
2026-04-28 10:41:51 -0600

    feat: variables de entorno Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,14 @@ import { getFirestore } from "firebase/firestore"
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBhZDB_APK2dON1mDonUn0RF9ooj-zAZvY",
-  authDomain: "mi-sistema-ventas-7e541.firebaseapp.com",
-  projectId: "mi-sistema-ventas-7e541",
-  storageBucket: "mi-sistema-ventas-7e541.firebasestorage.app",
-  messagingSenderId: "469018064019",
-  appId: "1:469018064019:web:296bc7fdc15e3e65734966"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 }
 
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
 export const auth = getAuth(app)
-
